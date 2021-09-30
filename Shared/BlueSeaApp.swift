154f4d60fb96e125921186e29c6d0bcefe918caa
Mike Willard <mikewillard87@gmail.com>
2021-09-30 15:19:53 -0400

    Initial Commit

---
+++ Shared/BlueSeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueSeaApp.swift
+//  Shared
+//
+//  Created by mike willard on 9/30/21.
+//
+
+import SwiftUI
+
+@main
+struct BlueSeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
